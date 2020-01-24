4ca25d68c35ef6720a56d5a5a57a54d2858241b8
Andrew Finke <andrewfinke@me.com>
2020-01-24 00:14:43 -0600

    icloud cleaning

--- components/icloud.py
+++ components/icloud.py
@@ -2,12 +2,14 @@ from pyicloud import PyiCloudService
 from pprint import pprint
 import config
 
-
 api = PyiCloudService(config.ICLOUD_USERNAME, config.ICLOUD_PASSWORD)
 
+
 def fetch_devices():
-    my_devices = [d for d in api.devices if d[config.ICLOUD_DEVICE_NAME_KEY].startswith(config.ICLOUD_DEVICE_NAME_PREFIX) and d[config.ICLOUD_DEVICE_TYPE_KEY] not in ICLOUD_FILTERED_DEVICE_TYPES]
-    my_devices = sorted(my_devices, key = lambda i: i[config.ICLOUD_DEVICE_NAME_KEY]) 
+    my_devices = [d for d in api.devices if d[config.ICLOUD_DEVICE_NAME_KEY].startswith(
+        config.ICLOUD_DEVICE_NAME_PREFIX) and d[config.ICLOUD_DEVICE_TYPE_KEY] not in config.ICLOUD_FILTERED_DEVICE_TYPES]
+    my_devices = sorted(
+        my_devices, key=lambda i: i[config.ICLOUD_DEVICE_NAME_KEY])
 
     formatted_devices = []
     for device in my_devices:
@@ -22,14 +24,16 @@ def fetch_devices():
 
     pprint(formatted_devices)
 
+
 def setup():
-    import click, sys
-    print ("Two-step authentication required. Your trusted devices are:")
+    import click
+    import sys
+    print("Two-step authentication required. Your trusted devices are:")
 
     devices = api.trusted_devices
     for i, device in enumerate(devices):
         print("  %s: %s" % (i, device.get('deviceName',
-            "SMS to %s" % device.get('phoneNumber'))))
+                                          "SMS to %s" % device.get('phoneNumber'))))
 
     device = click.prompt('Which device would you like to use?', default=0)
     device = devices[device]
@@ -43,8 +47,6 @@ def setup():
         sys.exit(1)
 
 
-
-
 # def fetch_events(calendar_name):
 #     calendar_api = api.calendar
 #     calendars = calendar_api.calendars()
@@ -53,7 +55,6 @@ def setup():
 #         if c['title'] == calendar_name:
 #             calender = c
 #             break
-    
+
 #     if not calender:
 #         raise SystemError('no calendar with name {}'.format(calendar_name))
-    
