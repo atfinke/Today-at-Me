04ac33d50c9d53a4f9a3857fd3a83d4f0b80ee40
Andrew Finke <andrewfinke@me.com>
2020-01-24 02:15:50 -0600

    logger fixes

--- components/logging.py
+++ components/logging.py
@@ -1,8 +1,22 @@
 import logging
+from pathlib import Path
+
+import config
 
 def setup_logger(name, log_file):
-    handler = logging.FileHandler(log_file)  
+    Path(config.LOGGING_DATA_PATH).mkdir(parents=True, exist_ok=True)
+
+    handler = logging.FileHandler(log_file) 
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+
     logger = logging.getLogger(name)      
     logger.setLevel(logging.DEBUG)
     logger.addHandler(logging.FileHandler(log_file))
+
+    streamer = logging.StreamHandler()
+    streamer.setLevel(logging.DEBUG)
+    streamer.setFormatter(formatter)
+    logger.addHandler(streamer)
+
     return logger
